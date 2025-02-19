4fb69b2dec9bea51e836f76c9757c7e194dfc45c
Shirakura-Kazuki <ok210192@oks.iput.ac.jp>
2025-02-19 18:32:28 +0900

    base.htmlのblock化

--- myDJ/urls.py
+++ myDJ/urls.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'helloApp/home.html')
 
 urlpatterns = [
     path('', home, name='home'),  # トップページの設定
